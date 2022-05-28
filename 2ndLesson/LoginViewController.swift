96d0530ce67fcc27ce6ab9f51cc85efe0f381993
RomanVakulenko <vakula_48@mail.ru>
2022-05-28 09:38:36 +0300

    scrollView & recognizer

--- 2ndLesson/LoginViewController.swift
+++ 2ndLesson/LoginViewController.swift
@@ -8,53 +8,42 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    
-    
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passwordtextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
   
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+//Вариант с лекции:
 //        let recognizer = UITapGestureRecognizer(target: self, action: #selector(onTap))
 //        self.view.addGestureRecognizer(recognizer)
-//
+        
         let recognizerTwo = UILongPressGestureRecognizer(target: self, action: #selector(onPush))
         self.view.addGestureRecognizer(recognizerTwo)
     }
-//
-//    @objc func onTap () {
-//        print("tap")
-//    }
-    
-    @objc func onPush (_ at: UIButton) {
-        if let login = userNameTextField.text,
-            login == "some" {
-            passwordtextField.text = "longPressGestureWorks"
-            }
-        }   
+//По долгому нажатию поле Name заполнится текстом "longPressGestureWorks"
+    @objc func onPush () {
+//        print("tap") - Варинат с лекции
+        userNameTextField.text = "longPressGestureWorks"
+    }
+
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+
+    if let login = userNameTextField.text,
+        login == "root" {
+            userNameTextField.backgroundColor = UIColor.green
+        }
+        else {
+            userNameTextField.backgroundColor = UIColor.magenta
+        }
+    if let password = passwordtextField.text,
+        password == "123" {
+        passwordtextField.backgroundColor = UIColor.green
+        }
+        else {
+            passwordtextField.backgroundColor = UIColor.magenta
+        }
+    }
 }
-//    @IBAction func loginButtonPressed(_ sender: UIButton) {
-//
-//    if let login = userNameTextField.text,
-//        login == "root" {
-//            userNameTextField.backgroundColor = UIColor.green
-//        }
-//        else {
-//            userNameTextField.backgroundColor = UIColor.magenta
-//        }
-//    if let password = passwordtextField.text,
-//        password == "123" {
-//        passwordtextField.backgroundColor = UIColor.green
-//        }
-//        else {
-//            passwordtextField.backgroundColor = UIColor.magenta
-//        }
-//
-//    }
-//}
 
