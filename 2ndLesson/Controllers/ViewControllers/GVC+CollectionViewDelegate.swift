3d9218b1c6de800b01ec45078b74783baf7b8872
RomanVakulenko <vakula_48@mail.ru>
2022-06-28 08:58:18 +0300

    9.1.OpenFotoToTheFullScreen

--- 2ndLesson/Controllers/ViewControllers/GVC+CollectionViewDelegate.swift
+++ 2ndLesson/Controllers/ViewControllers/GVC+CollectionViewDelegate.swift
@@ -11,21 +11,21 @@ extension GallaryViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print(indexPath.item)
         
-        //9.1 FullScreen зададим фрейм во весь экран
-        let fullScreenView = UIView(frame: self.view.bounds) //создадим Вью
-        let fullScreenImageView = UIImageView(frame: fullScreenView.frame) //надо добавить ImageView
-        fullScreenView.addSubview(fullScreenImageView)// добавляем на фуллСкринВью ImageView
+//9.1 FullScreen зададим фрейм во весь экран
+        let fullScreenView = UIView(frame: self.view.bounds) //создадим fullScreenView
+        let fullScreenImageView = UIImageView(frame: fullScreenView.frame) //создадим ImageView
+        fullScreenView.addSubview(fullScreenImageView)// добавляем ImageView на fullScreenView
         
         let imageFullScreen = Storage.shared.friends[fotoAlbumIndex].fotoAlbum[indexPath.item].url //возьмем из Сторадж, добавили url,чтобы в строке ниже named было действиетльно строка
         fullScreenImageView.image = UIImage(named: imageFullScreen) // берем картинку (можно взять по имени, можно взять системную картинку systemName, можно взять из url)
-        self.view.addSubview(fullScreenView) // 14m добавляем на Вью
+        self.view.addSubview(fullScreenView) // 14m добавляем fullScreenView на абсолютно отдельную Вью (типо вызываем)
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(onTap(_:))) // создадим tap
         fullScreenView.addGestureRecognizer(tap)// 20m добавляем на Вью tap
     }
-    //нам нужны будут параметры, чтобы выцепить из него Вьюшку
+    //нам нужны будут параметры(UITapGestureRecognizer), чтобы выцепить из него Вьюшку
     @objc func onTap(_ recognizer: UITapGestureRecognizer) {
-        guard let targetView = recognizer.view else {return}
+        guard let targetView = recognizer.view else {return} //тк recognizer.view опционал, то его надо развернуть
         targetView.removeFromSuperview()
     }
 }
